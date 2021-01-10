9f460eb77eacb380d2e5301a6ca6cf1acb7e60f2
nakayan5 <tomoya.progress@gmail.com>
2021-01-10 23:03:52 +0900

    usecontextを追加

---
+++ usecontext/src/App.js
@@ -0,0 +1,33 @@
+// ReactからcreateContextとuseStateをimport
+import React, {createContext, useState} from 'react'
+import './App.css';
+import ContextA from './components/ContextSample/ContextA'
+
+//createContextを使ってUserContextとHobbyContextを作成
+export const UserContext = createContext()
+export const HobbyContext = createContext()
+
+function App() {
+
+//useStateを使ってuserを作成
+  const [user, setUser] = useState({
+    name: 'セイラ',
+    age: '17'
+  })
+//useStateを使ってhobbyを作成
+  const [hobby, setHobby] = useState('キャンプ')
+
+  return (
+    <div className='App'>
+{/* userContext。Providerを作成、valueにはuserをセット */}
+      <UserContext.Provider value={user}>
+{/* HobbyContext.Providerを作成、valueにはhobbyをセット */}
+        <HobbyContext.Provider value={hobby}>
+          <ContextA/>
+        </HobbyContext.Provider>
+      </UserContext.Provider>
+    </div>
+  )
+}
+
+export default App
